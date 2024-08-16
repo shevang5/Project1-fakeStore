8c21695f31f9e32a7e968c945a5af7de2039d1fc
shevang <ffff>
2024-08-16 12:43:22 +0530

    change2

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,13 @@ import App from "./App.jsx";
 import "./index.css";
 import { HashRouter } from "react-router-dom";
 import Context from "./utils/Context.jsx";
-
+//fakestore
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Context>
 
   <HashRouter>
+    
     <App />
   </HashRouter>
   </Context>
