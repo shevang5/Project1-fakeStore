9181d30ab66a9051609f233f7f24e958e5c755c0
shevang <ffff>
2024-08-16 12:33:58 +0530

    change 1

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Context from "./utils/Context.jsx";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Context>
 
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
   </Context>
 );
